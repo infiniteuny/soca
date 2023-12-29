74eed226622f3f2e0ca16ac0492b427504b0ff2a
Muhammad Naufal <me@falkia34.dev>
2023-12-29 22:22:54 +0700

    feat: Add env example file
    Signed-off-by: Muhammad Naufal <me@falkia34.dev>

--- config/settings.py
+++ config/settings.py
@@ -1,7 +1,9 @@
-from pydantic_settings import BaseSettings
+from pydantic_settings import BaseSettings, SettingsConfigDict
 
 
 class Settings(BaseSettings):
+    model_config = SettingsConfigDict(env_file=".env")
+
     app_name: str = "Soca"
     app_description: str = "Soca is an API wrapper for a CCTV NVR."
     rtsp_host: str = "localhost"
