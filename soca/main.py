501d86d044386418e35f24e8df69efe13b6652ec
Muhammad Naufal <me@falkia34.dev>
2023-12-31 16:14:42 +0700

    feat(middleware): Add CORS middleware
    Signed-off-by: Muhammad Naufal <me@falkia34.dev>

--- soca/main.py
+++ soca/main.py
@@ -1,5 +1,6 @@
 from contextlib import asynccontextmanager
 from fastapi import APIRouter, FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from soca.application.stream_camera import StreamCamera
 from soca.infrastructure.datasources.rtsp_datasource import RtspDataSource
 from soca.infrastructure.repositories.rtsp_repository import RtspRepository
@@ -29,17 +30,25 @@ app = FastAPI(
     lifespan=lifespan,
 )
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=['*'],
+    allow_credentials=True,
+    allow_methods=['*'],
+    allow_headers=['*'],
+)
+
 # API router
 api = APIRouter(
-    prefix="/api",
+    prefix='/api',
 )
 
 # Add API router
-api.include_router(livestream, prefix="/v1")
-api.include_router(capture, prefix="/v1")
-api.include_router(snapshot, prefix="/v1")
+api.include_router(livestream, prefix='/v1')
+api.include_router(capture, prefix='/v1')
+api.include_router(snapshot, prefix='/v1')
 app.include_router(api)
 
 
 if __name__ == '__main__':
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    uvicorn.run(app, host='0.0.0.0', port=8000)
