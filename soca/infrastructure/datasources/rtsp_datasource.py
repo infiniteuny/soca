6a4570ed7ba5d32337148ab1b6f119f31b11f3fd
Muhammad Naufal <me@falkia34.dev>
2024-01-25 01:10:03 +0700

    feat(datasource): Get captures from the latest livestreams video chunk
    Signed-off-by: Muhammad Naufal <me@falkia34.dev>

--- soca/infrastructure/datasources/rtsp_datasource.py
+++ soca/infrastructure/datasources/rtsp_datasource.py
@@ -1,12 +1,15 @@
 from base64 import b64encode
 from datetime import datetime
 from fastapi import Depends
+from pathlib import Path
 from soca.config.settings import Settings
 from soca.injection import settings
-from time import time
+from time import sleep, time
 from typing import Annotated
 from vidgear.gears import CamGear, StreamGear, WriteGear
 import cv2
+import shutil
+import subprocess
 import os
 
 
@@ -70,44 +73,64 @@ class RtspDataSource:
         # Construct the RTSP URL
         self.__construct_rtsp_url(camera_id)
 
+        # Construct the input path
+        input_path = os.path.join(
+            self.settings.app_temp_dir, 'streams', camera_id)
+        files = [Path(f) for f in Path(input_path).glob(
+            '*.ts') if os.path.isfile(f)]
+        sorted_files = sorted(
+            files, key=lambda x: os.path.getmtime(x))
+
+        if not sorted_files:
+            raise Exception(
+                f'Could not find any stream files for camera ID {camera_id}.'
+            )
+
+        input_file = sorted_files.pop()
+
         # Construct the output path
-        output_path = os.path.join(self.settings.app_temp_dir, 'captures')
-        output_file = os.path.join(output_path, '{camera_id}.mp4')
+        output_path = os.path.join(
+            self.settings.app_temp_dir, 'captures', camera_id
+        )
+        output_file = os.path.join(output_path, f'{input_file.stem}.mp4')
+        lock_file = os.path.join(output_path, '.lock')
 
         # Create ouput directory if it does not exist
         if not os.path.exists(output_path):
             os.makedirs(output_path)
 
-        # Open the stream
-        source = CamGear(source=self.rtsp_url).start()  # type: ignore
+        # Check if the output file does not exist
+        if not os.path.isfile(output_file):
+            # Check if lock_file is exist every half second, but timeout after 3 seconds
+            start_time = time()
+            while os.path.isfile(lock_file):
+                sleep(0.5)
 
-        output_params = {
-            '-input_framerate': source.framerate,
-            '-c:v': 'libx264',
-        }
+                if time() - start_time > 3:
+                    break
 
-        # Define writer with defined parameters and suitable output filename
-        writer = WriteGear(
-            output=output_file,
-            **output_params  # type: ignore
-        )
+            # Remove and recreate the output directory
+            shutil.rmtree(output_path, ignore_errors=True)
+            os.makedirs(output_path)
 
-        end_time = time() + duration + 1
-        while time.time() < end_time:
-            # Read frames
-            frame = source.read()
+            # Write the lock file
+            with open(lock_file, 'w') as _:
+                pass
 
-            # Save the frame
-            writer.write(frame)
-
-        # Safely close video stream
-        source.stop()
-
-        # Safely close writer
-        writer.close()
+            # Execute FFmpeg command
+            subprocess.Popen(
+                f'ffmpeg -i "{input_file}" -c:v copy "{output_file}"',
+                shell=True,
+                stdout=subprocess.DEVNULL,
+                stderr=subprocess.STDOUT
+            ).wait()
 
         with open(output_file, 'rb') as video_file:
             encoded_video = b64encode(video_file.read())
+
+        # Remove the lock file
+        if os.path.isfile(lock_file):
+            os.remove(lock_file)
 
         return encoded_video.decode('utf-8')
 
